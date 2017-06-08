7ae17351b8bc51ac0f301dda272d24d6dd71ad5c
Rakesh Kumar Shah <rakesh.shah.iiita@gmail.com>
2017-06-07 23:31:59 -0500

    Palindrome Permutation

---
+++ PalindromePermutation/src/palindromepermutation/PalindromePermutation.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palindromepermutation;
+
+/**
+ *
+ * @author Rakesh
+ */
+public class PalindromePermutation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String str = "taco cat";
+        int count = 0;
+        boolean flag =false;
+        int[] charArr = new int[128]; //use Hashmap to reduce the space complexity
+        for(int i=0; i< str.length();i++){
+            if(str.charAt(i) != ' '){
+                charArr[str.charAt(i)] += 1; 
+            }
+        }
+
+        for(int i = 0; i<128;i++){
+            if(charArr[i] != 0){
+                if(charArr[i] %2 ==0){
+                        flag = true;
+                }
+                else{
+                    if(((charArr[i])-1) % 2 ==0){
+                        flag =true; 
+                        count++;
+                    }
+                    else if(charArr[i] == 1){
+                        count++;   
+                    }
+                    else 
+                        flag = false;
+                }
+            }
+        }
+
+        if(flag && count == 1){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+
+        }
+    }
+}
